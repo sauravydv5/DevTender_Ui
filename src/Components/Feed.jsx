8d7ea18983e21d41e7bade764f0506ce3defd5cc
saurav kumar <sauravkumar91937@gmail.com>
2025-05-25 22:57:18 +0530

    add

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { BASE_URL } from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
 
 const Feed = () => {
-  return <div>feed</div>;
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data));
+    } catch (err) {
+      console.error("Error fetching feed:", err.response?.status, err.message);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return (
+    <div>
+      {feed ? (
+        <pre>{JSON.stringify(feed, null, 2)}</pre>
+      ) : (
+        <p>Loading feed...</p>
+      )}
+    </div>
+  );
 };
 
 export default Feed;
